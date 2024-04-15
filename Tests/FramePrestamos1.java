844b012f4759a3bef27ec15a51cfbd192b73985b
FrancoCC6 <francocalzettacampos@hotmail.com>
2024-04-14 21:30:58 -0300

    Layout nuevo prestamo 1 (Aparte)

---
+++ Tests/FramePrestamos1.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class FramePrestamos1 {
+	//private static ActionListener validate_and_
+	private static void buildFrame() {
+		JFrame frame = new JFrame();
+		frame.setLayout(new GridBagLayout());
+		JTextField
+			input_identifier = new JTextField(20),
+			input_element = new JTextField(15),
+			input_quantity = new JTextField(5);
+		JButton button_aceptar = new JButton("Aceptar");
+
+		GridBagConstraints gbc = new GridBagConstraints();
+
+		gbc.fill = GridBagConstraints.HORIZONTAL;
+
+		gbc.gridwidth = 2;
+
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+
+		frame.add(new JLabel("Nombre y Apellido/DNI"), gbc);
+
+		gbc.gridy = 1;
+		frame.add(input_identifier, gbc);
+
+		gbc.gridwidth = 1;
+
+		gbc.gridy = 2;
+		frame.add(new JLabel("Elemento"), gbc);
+
+		gbc.gridy = 3;
+		frame.add(input_element, gbc);
+
+		gbc.gridy = 2;
+		gbc.gridx = 1;
+		frame.add(new JLabel("Cdad."), gbc);
+
+		gbc.gridy = 3;
+		frame.add(input_quantity, gbc);
+
+		gbc.weighty = .5;
+		gbc.gridwidth = 2;
+
+		gbc.fill = GridBagConstraints.NONE;
+		gbc.gridy = 4;
+		gbc.gridx = 0;
+		frame.add(button_aceptar, gbc);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Nuevo Prestamo");
+		frame.setSize(new Dimension(250, 150));
+		frame.setResizable(false);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> buildFrame());
+	}
+}
